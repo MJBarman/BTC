d91958800770d10ac53f01d41673fba3fd6518f3
manash <mjbisover@gmail.com>
2022-11-09 13:23:53 +0530

    added radio button

--- app/src/main/java/com/amtron/btc/ui/EnterDetailsActivity.kt
+++ app/src/main/java/com/amtron/btc/ui/EnterDetailsActivity.kt
@@ -7,9 +7,13 @@ import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import android.widget.Spinner
 import android.widget.Toast
 import com.amtron.btc.R
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class EnterDetailsActivity : AppCompatActivity() {
 
@@ -17,20 +21,51 @@ class EnterDetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_enter_details)
+        val name: TextInputEditText = findViewById(R.id.name)
+        val rg_gender: RadioGroup = findViewById(R.id.rg_gender)
+        val rbMale: RadioButton = findViewById(R.id.rb_male)
+        val rbFemale: RadioButton = findViewById(R.id.rb_female)
+        val age: TextInputEditText = findViewById(R.id.age)
+        val rgNationality: RadioGroup = findViewById(R.id.rg_nationality)
+        val rbIndian: RadioButton = findViewById(R.id.rb_indian)
+        val rbForeign: RadioButton = findViewById(R.id.rb_foreign)
+        val residenSpinner: AutoCompleteTextView = findViewById(R.id.spinner_state_dropdown)
+        val state: TextInputLayout = findViewById(R.id.spinner_state)
 
-
-        val residenSpinner: AutoCompleteTextView = findViewById(R.id.spinner_residency_indian)
         val listResidency = ArrayList<String>()
         listResidency.add("Hatigaon")
         listResidency.add("Beltola")
-        listResidency.add("Six Nile")
+        listResidency.add("Six Mile")
         listResidency.add("Ganeshguri")
 
-        val numberAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, listResidency)
+        val numberAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item,
+            listResidency)
 
         residenSpinner.setAdapter(numberAdapter)
         residenSpinner.setOnItemClickListener { adapterView, view, i, l ->
-            Toast.makeText(this, adapterView.getItemAtPosition(i).toString(), Toast.LENGTH_LONG).show()
+            Toast.makeText(this, adapterView.getItemAtPosition(i).toString(),
+                Toast.LENGTH_LONG).show()
+        }
+
+        fun onRadioButtonClicked(view: View) {
+            if (view is RadioButton) {
+                // Is the button now checked?
+                val checked = view.isChecked
+
+                // Check which radio button was clicked
+                when (view.getId()) {
+                    R.id.rb_indian ->
+                        if (checked) {
+                            Toast.makeText(this, "Indian",
+                                Toast.LENGTH_LONG).show()
+                        }
+                    R.id.rb_foreign ->
+                        if (checked) {
+                            Toast.makeText(this, "Foreign",
+                                Toast.LENGTH_LONG).show()
+                        }
+                }
+            }
         }
 
     }
