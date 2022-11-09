c9a5ce1945052c737987e0d93b1ef3ad0986212f
pranz1001 <debabrata.sarma2012@gmail.com>
2022-11-09 12:43:56 +0530

    update

--- app/src/main/java/com/amtron/btc/model/MasterData.kt
+++ app/src/main/java/com/amtron/btc/model/MasterData.kt
@@ -1,10 +1,16 @@
 package com.amtron.btc.model
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
 data class MasterData (
-    val name: String?,
-    val age: Int?,
-    val gender: String?,
-    val country: String?,
-    val state: String?,
-    val division: String
+    @PrimaryKey val masterId: Int,
+    @ColumnInfo(name = "name") val name: String?,
+    @ColumnInfo(name = "age") val age: Int?,
+    @ColumnInfo(name = "gender") val gender: String?,
+    @ColumnInfo(name = "country") val country: String?,
+    @ColumnInfo(name = "state") val state: String?,
+    @ColumnInfo(name = "division") val division: String
 )
