9f2891a96d10c0ffde9f74b3dec6194e26b2cf8b
manash <mjbisover@gmail.com>
2022-11-10 12:10:54 +0530

    xml changes

--- app/src/main/java/com/amtron/btc/ui/EnterDetailsActivity.kt
+++ app/src/main/java/com/amtron/btc/ui/EnterDetailsActivity.kt
@@ -6,18 +6,16 @@ import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import com.amtron.btc.R
-import com.amtron.btc.databinding.ActivityEnterDetailsBinding
-import com.google.android.material.button.MaterialButton
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 
 class EnterDetailsActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityEnterDetailsBinding
 
     @SuppressLint("WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_enter_details)
+        val button: Button = findViewById(R.id.add_data)
         val name: TextInputEditText = findViewById(R.id.name)
         val rg_gender: RadioGroup = findViewById(R.id.rg_gender)
         val rbMale: RadioButton = findViewById(R.id.rb_male)
@@ -29,7 +27,10 @@ class EnterDetailsActivity : AppCompatActivity() {
         val residenSpinner: AutoCompleteTextView = findViewById(R.id.spinner_state_dropdown)
         val state: TextInputLayout = findViewById(R.id.spinner_state)
         val country: TextInputLayout = findViewById(R.id.spinner_country)
-        val proceed: MaterialButton = findViewById(R.id.add_data)
+
+        button.setOnClickListener {
+            //on click
+        }
 
         val listResidency = ArrayList<String>()
         listResidency.add("Assam")
@@ -70,8 +71,5 @@ class EnterDetailsActivity : AppCompatActivity() {
             showHide(country)
         }
 
-        proceed.setOnClickListener {
-            Toast.makeText(this, "add data", Toast.LENGTH_SHORT).show()
-        }
     }
 }
